198d2dcca66e9b525d2858c78e34403ee50a2b2b
alperenkavukk <kavuk.67.76@gmail.com>
2022-09-24 20:01:41 +0300

    Initial Commit

---
+++ ZarGame/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ZarGame
+//
+//  Created by Alperen Kavuk on 24.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
